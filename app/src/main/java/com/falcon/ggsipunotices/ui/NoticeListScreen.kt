40fa72b4ca51080e758b19696f27b14632af42d5
Avishisht Gupta <avishishtgupta@gmail.com>
2024-07-26 01:56:07 +0530

    Preview

--- app/src/main/java/com/falcon/ggsipunotices/ui/NoticeListScreen.kt
+++ app/src/main/java/com/falcon/ggsipunotices/ui/NoticeListScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.BasicTextField
@@ -19,21 +20,32 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.falcon.ggsipunotices.utils.Resource
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+private fun PreviewNoticeListScreen() {
+    NoticeListScreen()
+}
 
 @Composable
 fun NoticeListScreen(mainViewModel: MainViewModel = hiltViewModel()) {
     val notices by mainViewModel.notices.collectAsState()
     var searchQuery by remember { mutableStateOf(TextFieldValue("")) }
 
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
         BasicTextField(
             value = searchQuery,
             onValueChange = { searchQuery = it },
             modifier = Modifier
                 .fillMaxWidth()
+                .height(100.dp)
                 .background(Color.Gray)
                 .padding(8.dp)
         )
